1b3ba42811fbdb8b5b7bbcc47ac87fd927d56bdb
Elliot Tormey <elliot.tormey@gmail.com>
2021-08-28 14:59:41 +0100

    update AGP to 7.1.0-alpha10 and gradle wrapper to 7.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ buildscript {
   }
 
   dependencies {
-    classpath("com.android.tools.build:gradle:7.1.0-alpha08")
+    classpath("com.android.tools.build:gradle:7.1.0-alpha10")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
     classpath("org.jetbrains.kotlin:kotlin-serialization:1.5.21")
     classpath("com.squareup.sqldelight:gradle-plugin:1.5.1")
@@ -37,4 +37,9 @@ allprojects {
     google()
     mavenCentral()
   }
+}
+
+tasks.withType<Wrapper> {
+  gradleVersion = "7.2"
+  distributionType = Wrapper.DistributionType.BIN
 }
